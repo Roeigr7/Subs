5d3c64e4bc545cb9dc2bff44629db02a760331a3
Daniel Levi <danielldev1@gmail.com>
2024-06-10 10:19:22 +0300

    added paypal pricing plan

--- src/modules/components/PricingPlan.js
+++ src/modules/components/PricingPlan.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -8,6 +8,23 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
 export default function PricingPlan() {
+  const paypalOptions = {
+    'clientId': 'AZ4S4oMrteLjp4tO_TEuSY1AxfWDWh4JKh59heFg1cLyBdIh_iQcFg99eahaxTqtTG0A99DschWBrk_o',
+    currency: 'USD',
+  };
+
+  const handleApprove = (data, actions) => {
+    return actions.order.capture().then(details => {
+      console.log("Payment approved:", details);
+      // Handle successful payment here
+    });
+  };
+
+  const handleError = (err) => {
+    console.error("Payment error:", err);
+    // Handle payment error here
+  };
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -25,7 +42,23 @@ export default function PricingPlan() {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">buy now</Button>
+        <PayPalScriptProvider options={paypalOptions}>
+          <PayPalButtons
+            createOrder={(data, actions) => {
+              return actions.order.create({
+                intent: 'CAPTURE', // Corrected placement of the intent property
+                purchase_units: [{
+                  amount: {
+                    currency_code: 'USD',
+                    value: '1.00'
+                  }
+                }],
+              });
+            }}
+            onApprove={handleApprove}
+            onError={handleError}
+          />
+        </PayPalScriptProvider>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
